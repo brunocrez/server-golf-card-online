9785db186b830b51418e776aecba8c7fa5c8bd68
Bruno Rezende <brunoc.rez@gmail.com>
2024-10-10 17:08:52 -0300

    chore: socket.io cors config

--- src/server.ts
+++ src/server.ts
@@ -13,27 +13,29 @@ import {
 const PORT = process.env.PORT ? Number(process.env.PORT) : 8080
 
 const fastify = Fastify()
-const io = new Server(fastify.server, { cors: { origin: '*' } })
-
 fastify.register(cors, { origin: '*' })
 
 const lobbies = new Map<string, ILobby>()
-
-io.on('connection', (socket) => {
-  createLobby(socket, lobbies)
-  joinLobby(socket, lobbies)
-  getLobby(socket, lobbies)
-  leaveLobby(socket, lobbies)
-
-  socket.on('disconnect', () => {
-    onDisconnect(socket, lobbies)
-  })
-})
 
 const start = async () => {
   try {
     await fastify.listen({ port: PORT })
     console.log(`Server running at PORT ${PORT}`)
+
+    const io = new Server(fastify.server, {
+      cors: { origin: '*', methods: ['GET', 'POST'], credentials: true },
+    })
+
+    io.on('connection', (socket) => {
+      createLobby(socket, lobbies)
+      joinLobby(socket, lobbies)
+      getLobby(socket, lobbies)
+      leaveLobby(socket, lobbies)
+
+      socket.on('disconnect', () => {
+        onDisconnect(socket, lobbies)
+      })
+    })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
