b11f11a4b53e0c1dca7ad387b108e05383416aab
Bruno Rezende <brunoc.rez@gmail.com>
2024-11-12 16:28:07 -0300

    feat: set end game function

--- src/sockets/gameHandler.ts
+++ src/sockets/gameHandler.ts
@@ -1,5 +1,5 @@
 import { Socket } from 'socket.io'
-import { ILobby } from '../models/lobby'
+import { ILobby, IScoreBoard } from '../models/lobby'
 import {
   CARDS_PER_PLAYER,
   cardScore,
@@ -15,9 +15,9 @@ import {
   updatePlayerBoard,
   updatePlayerMoves,
 } from '../utils/game'
-import { createNewDeck, drawCard } from '../services/deck-service'
+import { createNewDeck, drawCard, shuffleDeck } from '../services/deck-service'
 import { LobbyStatus } from '../utils/LobbyStatus'
-import { Card } from '../models/deck'
+import { Card, IDeck } from '../models/deck'
 
 interface IFlipCardRequest {
   playerId: string
@@ -68,7 +68,6 @@ export const gameHandler = (socket: Socket, lobbies: Map<string, ILobby>) => {
           ...lobby,
           players: playersWithCards,
           deck: { ...deck, remaining },
-          isFirstTurn: true,
           discardPile: [discardCard],
           status: LobbyStatus.IN_PROGRESS,
           currentTurn: lobby.host,
@@ -107,15 +106,11 @@ export const gameHandler = (socket: Socket, lobbies: Map<string, ILobby>) => {
         players: updatedPlayers,
       }
 
-      lobbies.set(id, updateGameState)
-
-      if (startLastTurn(lobby, playerId)) {
+      if (startLastTurn(updateGameState, playerId)) {
         updateGameState = {
           ...lobby,
           playerStartedLastTurn: playerId,
         }
-
-        lobbies.set(id, updateGameState)
       }
 
       if (isLastTurn(players)) {
@@ -128,17 +123,14 @@ export const gameHandler = (socket: Socket, lobbies: Map<string, ILobby>) => {
         if (players[nextTurnIndex].playerId === playerStartedLastTurn) {
           // chegou na pessoa que virou a última carta
           // chamar função que finaliza o game endGame()
+          socket.emit('finish-round', { message: 'finishing round ...' })
+          socket.broadcast
+            .to(id)
+            .emit('finish-round', { message: 'finishing round ...' })
 
-          updateGameState = {
-            ...lobby,
-            players: updatePlayer,
-          }
-
-          lobbies.set(lobby.id, updateGameState)
-          socket.emit('updated-game', updateGameState)
-          socket.broadcast.to(lobby.id).emit('updated-game', updateGameState)
-
-          return
+          setTimeout(() => {
+            endGame(lobby)
+          }, 5000)
         }
 
         updateGameState = {
@@ -147,14 +139,15 @@ export const gameHandler = (socket: Socket, lobbies: Map<string, ILobby>) => {
           currentTurn: lobby.players[nextTurnIndex].playerId,
         }
 
-        lobbies.set(lobby.id, updateGameState)
+        lobbies.set(id, updateGameState)
         socket.emit('updated-game', updateGameState)
-        socket.broadcast.to(lobby.id).emit('updated-game', updateGameState)
+        socket.broadcast.to(id).emit('updated-game', updateGameState)
         return
       }
 
       if (isFirstTurn(lobby.players)) {
         const updatedPlayerMoves = updatePlayerMoves(updatedPlayers, playerId)
+
         updateGameState = {
           ...lobby,
           players: updatedPlayerMoves,
@@ -168,31 +161,29 @@ export const gameHandler = (socket: Socket, lobbies: Map<string, ILobby>) => {
             currentTurn: lobby.players[nextTurnIndex].playerId,
           }
 
-          lobbies.set(lobby.id, updateGameState)
+          lobbies.set(id, updateGameState)
           socket.emit('updated-game', updateGameState)
-          socket.broadcast.to(lobby.id).emit('updated-game', updateGameState)
+          socket.broadcast.to(id).emit('updated-game', updateGameState)
           return
         }
 
-        lobbies.set(lobby.id, updateGameState)
+        lobbies.set(id, updateGameState)
         socket.emit('updated-game', updateGameState)
-        socket.broadcast.to(lobby.id).emit('updated-game', updateGameState)
+        socket.broadcast.to(id).emit('updated-game', updateGameState)
         return
       }
 
       const nextTurnIndex = nextTurn(lobby, playerId)
 
       updateGameState = {
-        ...lobby,
+        ...updateGameState,
         players: updatedPlayers,
         currentTurn: lobby.players[nextTurnIndex].playerId,
       }
 
-      lobbies.set(lobby.id, updateGameState)
+      lobbies.set(id, updateGameState)
       socket.emit('updated-game', updateGameState)
-      socket.broadcast.to(lobby.id).emit('updated-game', updateGameState)
-
-      return
+      socket.broadcast.to(id).emit('updated-game', updateGameState)
     })
   }
 
@@ -229,16 +220,12 @@ export const gameHandler = (socket: Socket, lobbies: Map<string, ILobby>) => {
         discardPile: updatedDiscardPile,
       }
 
-      lobbies.set(lobbyId, updateGameState)
-
-      if (startLastTurn(lobby, playerId)) {
+      if (startLastTurn(updateGameState, playerId)) {
         updateGameState = {
           ...lobby,
           playerStartedLastTurn: playerId,
           discardPile: updatedDiscardPile,
         }
-
-        lobbies.set(id, updateGameState)
       }
 
       if (isLastTurn(players)) {
@@ -251,18 +238,14 @@ export const gameHandler = (socket: Socket, lobbies: Map<string, ILobby>) => {
         if (players[nextTurnIndex].playerId === playerStartedLastTurn) {
           // chegou na pessoa que virou a última carta
           // chamar função que finaliza o game endGame()
+          socket.emit('finish-round', { message: 'finishing round ...' })
+          socket.broadcast
+            .to(id)
+            .emit('finish-round', { message: 'finishing round ...' })
 
-          updateGameState = {
-            ...lobby,
-            players: updatePlayer,
-            discardPile: updatedDiscardPile,
-          }
-
-          lobbies.set(lobby.id, updateGameState)
-          socket.emit('updated-game', updateGameState)
-          socket.broadcast.to(lobby.id).emit('updated-game', updateGameState)
-
-          return
+          setTimeout(() => {
+            endGame(lobby)
+          }, 5000)
         }
 
         updateGameState = {
@@ -272,9 +255,9 @@ export const gameHandler = (socket: Socket, lobbies: Map<string, ILobby>) => {
           discardPile: updatedDiscardPile,
         }
 
-        lobbies.set(lobby.id, updateGameState)
+        lobbies.set(id, updateGameState)
         socket.emit('updated-game', updateGameState)
-        socket.broadcast.to(lobby.id).emit('updated-game', updateGameState)
+        socket.broadcast.to(id).emit('updated-game', updateGameState)
         return
       }
 
@@ -296,32 +279,30 @@ export const gameHandler = (socket: Socket, lobbies: Map<string, ILobby>) => {
             discardPile: updatedDiscardPile,
           }
 
-          lobbies.set(lobby.id, updateGameState)
+          lobbies.set(id, updateGameState)
           socket.emit('updated-game', updateGameState)
-          socket.broadcast.to(lobby.id).emit('updated-game', updateGameState)
+          socket.broadcast.to(id).emit('updated-game', updateGameState)
           return
         }
 
-        lobbies.set(lobby.id, updateGameState)
+        lobbies.set(id, updateGameState)
         socket.emit('updated-game', updateGameState)
-        socket.broadcast.to(lobby.id).emit('updated-game', updateGameState)
+        socket.broadcast.to(id).emit('updated-game', updateGameState)
         return
       }
 
       const nextTurnIndex = nextTurn(lobby, playerId)
 
       updateGameState = {
-        ...lobby,
+        ...updateGameState,
         players: updatedPlayers,
         currentTurn: lobby.players[nextTurnIndex].playerId,
         discardPile: updatedDiscardPile,
       }
 
-      lobbies.set(lobby.id, updateGameState)
+      lobbies.set(id, updateGameState)
       socket.emit('updated-game', updateGameState)
-      socket.broadcast.to(lobby.id).emit('updated-game', updateGameState)
-
-      return
+      socket.broadcast.to(id).emit('updated-game', updateGameState)
     })
   }
 
@@ -349,7 +330,6 @@ export const gameHandler = (socket: Socket, lobbies: Map<string, ILobby>) => {
         }
 
         lobbies.set(id, updateGameState)
-
         const mappedCard = { ...cards[0], score: cardScore(cards[0]) }
 
         cb({ success: true, card: mappedCard })
@@ -378,16 +358,12 @@ export const gameHandler = (socket: Socket, lobbies: Map<string, ILobby>) => {
         discardPile: [...discardPile, card],
       }
 
-      lobbies.set(id, updateGameState)
-
-      if (startLastTurn(lobby, playerId)) {
+      if (startLastTurn(updateGameState, playerId)) {
         updateGameState = {
           ...lobby,
           playerStartedLastTurn: playerId,
           discardPile: [...discardPile, card],
         }
-
-        lobbies.set(id, updateGameState)
       }
 
       if (isLastTurn(players)) {
@@ -401,17 +377,14 @@ export const gameHandler = (socket: Socket, lobbies: Map<string, ILobby>) => {
           // chegou na pessoa que virou a última carta
           // chamar função que finaliza o game endGame()
 
-          updateGameState = {
-            ...lobby,
-            players: updatePlayer,
-            discardPile: [...discardPile, card],
-          }
+          socket.emit('finish-round', { message: 'finishing round ...' })
+          socket.broadcast
+            .to(id)
+            .emit('finish-round', { message: 'finishing round ...' })
 
-          lobbies.set(lobby.id, updateGameState)
-          socket.emit('updated-game', updateGameState)
-          socket.broadcast.to(lobby.id).emit('updated-game', updateGameState)
-
-          return
+          setTimeout(() => {
+            endGame(lobby)
+          }, 5000)
         }
 
         updateGameState = {
@@ -421,9 +394,9 @@ export const gameHandler = (socket: Socket, lobbies: Map<string, ILobby>) => {
           discardPile: [...discardPile, card],
         }
 
-        lobbies.set(lobby.id, updateGameState)
+        lobbies.set(id, updateGameState)
         socket.emit('updated-game', updateGameState)
-        socket.broadcast.to(lobby.id).emit('updated-game', updateGameState)
+        socket.broadcast.to(id).emit('updated-game', updateGameState)
         return
       }
 
@@ -460,7 +433,7 @@ export const gameHandler = (socket: Socket, lobbies: Map<string, ILobby>) => {
       const nextTurnIndex = nextTurn(lobby, playerId)
 
       updateGameState = {
-        ...lobby,
+        ...updateGameState,
         currentTurn: lobby.players[nextTurnIndex].playerId,
         discardPile: [...discardPile, card],
       }
@@ -468,13 +441,112 @@ export const gameHandler = (socket: Socket, lobbies: Map<string, ILobby>) => {
       lobbies.set(lobby.id, updateGameState)
       socket.emit('updated-game', updateGameState)
       socket.broadcast.to(lobby.id).emit('updated-game', updateGameState)
-
-      return
     })
+  }
+
+  const endGame = async (lobby: ILobby) => {
+    let updateGameState: ILobby = {} as ILobby
+    let deck: IDeck = {} as IDeck
+    let scoreBoard: IScoreBoard[] = []
+
+    // create current score board array
+    const currScoreBoard = lobby.players.map((player) => {
+      const { playerId, score, nickname } = player
+      return {
+        playerId,
+        score: score.reduce((acc, curr) => acc + curr, 0),
+        nickname,
+      }
+    })
+
+    // add current scoreBoard to existing one
+    if (lobby.scoreBoard) {
+      scoreBoard = lobby.scoreBoard
+        .map((lobbyScore) => {
+          const currScore = currScoreBoard.find(
+            (el) => el.playerId === lobbyScore.playerId,
+          )
+          return { ...lobbyScore, score: lobbyScore.score + currScore?.score! }
+        })
+        .sort((a, b) => a.score - b.score)
+    } else {
+      scoreBoard = currScoreBoard.sort((a, b) => a.score - b.score)
+    }
+
+    // reset players score
+    const players = lobby.players.map((player) => ({
+      ...player,
+      score: [0, 0, 0],
+    }))
+
+    // reshuffle the deck
+    try {
+      deck = await shuffleDeck(lobby.deck?.deck_id!)
+    } catch (error) {
+      console.error(error)
+      socket.emit('error-end-game', {
+        message:
+          'não foi possível finalizar o jogo, tente novamente mais tarde!',
+      })
+    }
+
+    updateGameState = {
+      ...lobby,
+      players,
+      scoreBoard,
+      deck,
+      currentRound: lobby.currentRound + 1,
+    }
+
+    lobbies.set(lobby.id, updateGameState)
+
+    // verify if it's the last round
+    if (lobby.currentRound + 1 > lobby.rounds) {
+      socket.emit('end-game', lobby)
+      return
+    }
+
+    proceedToNextRound(lobby.id)
+  }
+
+  const proceedToNextRound = async (lobbyId: string) => {
+    const lobby = lobbies.get(lobbyId)
+
+    if (!lobby) {
+      // algo deu errado!
+      return
+    }
+
+    const { deck } = lobby
+    const DRAW_COUNT = lobby.currentPlayers * CARDS_PER_PLAYER + 1
+
+    const { cards, remaining } = await drawCard(deck?.deck_id!, DRAW_COUNT)
+
+    const playersWithCards = distribuiteCards(lobby.players, cards)
+    const extraDrawnCard = cards[cards.length - 1]
+    const discardCard = {
+      ...extraDrawnCard,
+      score: cardScore(extraDrawnCard),
+    }
+
+    const updateGameState: ILobby = {
+      ...lobby,
+      players: playersWithCards,
+      deck: { ...deck!, remaining },
+      discardPile: [discardCard],
+      status: LobbyStatus.IN_PROGRESS,
+      currentTurn: lobby.host,
+    }
+
+    lobbies.set(lobby.id, updateGameState)
+    socket.emit('proceed-to-next-round', updateGameState)
+    socket.broadcast.to(lobby.id).emit('proceed-to-next-round', updateGameState)
   }
 
   return {
     startGame,
+    endGame,
+    proceedToNextRound,
     flipCard,
     replaceCard,
     drawCardFromDeck,
